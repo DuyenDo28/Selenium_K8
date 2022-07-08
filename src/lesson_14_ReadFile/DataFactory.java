449bbcea83b40ba1f4091b623389dda862bc2ea5
Do <duyen.do@datalogic.com>
2022-07-08 16:16:31 +0700

    lesson14 read and write a txt file

--- src/lesson_14_ReadFile/DataFactory.java
+++ src/lesson_14_ReadFile/DataFactory.java
@@ -10,7 +10,7 @@ public class DataFactory {
 
     public static void readFile(String filePath) {
         try (
-               // System.out.println("Reading file from".concat(filePath));
+                // System.out.println("Reading file from".concat(filePath));
                 // chỉ ra file input stream ở đâu, chỉ ra dòng suối ở đâu
                 FileInputStream fileInputStream = new FileInputStream(filePath);
                 // đưa vào ổng dẫn nước
@@ -18,16 +18,15 @@ public class DataFactory {
                 // đổ vô bình nước
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         ) {
-            String dataLine =bufferedReader.readLine();
-            while (dataLine !=null) {
+            String dataLine = bufferedReader.readLine();
+            while (dataLine != null) {
                 System.out.println(dataLine);
-                dataLine= bufferedReader.readLine();
+                dataLine = bufferedReader.readLine();
             }
 
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -36,7 +35,7 @@ public class DataFactory {
     // inputStreamReader.close();
     // fileInputStream.close();
 
-    public static List<Person> convertDataIntoPersonList(String filePath){
+    public static List<Person> convertDataIntoPersonList(String filePath) {
         List<Person> personList = new ArrayList<>();
 
         try (
@@ -48,71 +47,71 @@ public class DataFactory {
                 // đổ vô bình nước
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         ) {
-            String dataLine =bufferedReader.readLine();
-            while (dataLine !=null) {
-                String[] personData= dataLine.split(";");
+            String dataLine = bufferedReader.readLine();
+            while (dataLine != null) {
+                String[] personData = dataLine.split(";");
                 String personName = personData[0];
                 int personAge = Integer.parseInt(personData[1]);
-                int personSalary= Integer.parseInt(personData[2]);
-                Person person = new Person(personName,personAge,personSalary);
+                int personSalary = Integer.parseInt(personData[2]);
+                Person person = new Person(personName, personAge, personSalary);
                 personList.add(person);
-                dataLine= bufferedReader.readLine();
+                dataLine = bufferedReader.readLine();
             }
 
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return personList;
     }
 
-    public static void savePersonList(List<Person> personList, String filePath){
-        if(personList==null || filePath==null){
+    public static void savePersonList(List<Person> personList, String filePath) {
+        if (personList == null || filePath == null) {
             throw new IllegalArgumentException("Person list and filepath can't be null");
         }
-        if(personList.isEmpty())return;
+        if (personList.isEmpty()) return;
 
         try (
-            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-            OutputStreamWriter outputStreamWriter= new OutputStreamWriter(fileOutputStream);
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
         ) {
             for (Person person : personList) {
-                String personName =person.getName();
-                int personAge =person.getAge();
-                int personSalary =person.getSalary();
+                String personName = person.getName();
+                int personAge = person.getAge();
+                int personSalary = person.getSalary();
 
-                String dataLine = personName.concat(";").concat(String.valueOf(personAge)).concat(";").concat(String.valueOf(personSalary));
+                String dataLine =
+                        personName.concat(";").concat(String.valueOf(personAge)).concat(";").concat(String.valueOf(personSalary));
                 bufferedWriter.write(dataLine);
                 bufferedWriter.newLine();
-
-
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
-         String dataFileRelativePath ="\\src\\lesson_14_ReadFile\\data.txt";
-         String dataFileAbsolutePath = System.getProperty("user.dir").concat(dataFileRelativePath);
-       // List<Person> personList = DataFactory.convertDataIntoPersonList(dataFileAbsolutePath);
-       // System.out.println(personList);
+        // String dataFileRelativePath = "\\src\\lesson_14_ReadFile\\data.txt";
+        // String dataFileAbsolutePath = System.getProperty("user.dir").concat(dataFileRelativePath);
+        //  List<Person> personList = DataFactory.convertDataIntoPersonList(dataFileAbsolutePath);
+        // System.out.println(personList);
 
 
-      //  String dataFileRelativePath ="/src/lesson_14_ReadFile";
-       // String dataFileAbsolutePath = System.getProperty("user.dir").concat(dataFileRelativePath);
+        //  String dataFileRelativePath ="/src/lesson_14_ReadFile";
+        // String dataFileAbsolutePath = System.getProperty("user.dir").concat(dataFileRelativePath);
 
-        Person teo = new Person("teo",20, 100000);
-        Person ti = new Person("ti",22, 1100000);
+        String dataFileRelativePath = "\\src\\lesson_14_ReadFile\\person.txt";
+        String dataFileAbsolutePath = System.getProperty("user.dir").concat(dataFileRelativePath);
+
+        Person teo = new Person("teo", 20, 10000);
+        Person ti = new Person("ti", 22, 11000);
 
 
-        List<Person> employees = Arrays.asList(teo,ti);
+        List<Person> employees = Arrays.asList(teo, ti);
         DataFactory.savePersonList(employees, dataFileAbsolutePath);
-    }
 
+    }
 }
 
